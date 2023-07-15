9f0cd38ea5de1e5e18902b94ea2ba1293550351d
Martin <maguera37@gmail.com>
2023-07-15 16:03:00 -0300

    feat: add handleDeleteToDo in IToDo and IToDoModel interfaces

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -1,9 +1,11 @@
 export interface IToDo {
   id: number;
   toDo: string;
+  handleDeleteToDo: (id: IToDo['id']) => void
 }
 
 export interface IToDoModel {
   youWillDo: IToDo[];
   handleAddToDo: (toDo: IToDo) => void;
+  handleDeleteToDo: (id: IToDo['id']) => void
 }
