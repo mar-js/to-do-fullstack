cc4ca065109dc238e926bec19a65e982f8bbd463
Martin <maguera37@gmail.com>
2023-07-20 16:03:00 -0300

    feat: add handleDeleteFull in IToDoModel interface

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -1,11 +1,16 @@
 export interface IToDo {
   id: number;
   toDo: string;
-  handleDeleteToDo: (id: IToDo['id']) => void
+  handleDeleteToDo: (id: IToDo['id']) => void;
 }
 
 export interface IToDoModel {
   youWillDo: IToDo[];
   handleAddToDo: (toDo: IToDo) => void;
-  handleDeleteToDo: (id: IToDo['id']) => void
+  handleDeleteToDo: (id: IToDo['id']) => void;
+  handleDeleteFull: () => void;
+}
+
+export interface IDeleteFull {
+  handleDeleteFull: () => void;
 }
