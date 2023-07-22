64a19522dc136d4471454ebf2a25e614fb086f40
Martin <maguera37@gmail.com>
2023-07-22 16:03:00 -0300

    feat: add required to the input for create an to do

--- client/src/components/form/index.tsx
+++ client/src/components/form/index.tsx
@@ -20,7 +20,7 @@ export const Form: React.FC = () => {
 
   return (
     <form action="GET" className="w-full flex justify-center items-center flex-col mb-7" onSubmit={ handleSubmit }>
-      <input type="text" placeholder="Eat" name="toDo" className="w-[60rem] p-4 text-xl border mb-5" />
+      <input type="text" placeholder="Eat" name="toDo" className="w-[60rem] p-4 text-xl border mb-5" required />
       <button type="submit" className="bg-purple-500 text-white text-2xl px-5 py-2 rounded hover:bg-purple-800 hover:shadow-lg">Create</button>
     </form>
   )
