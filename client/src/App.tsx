d26f06deac40d1799b4bafd01c8da7364bcecfb2
Martin <maguera37@gmail.com>
2023-07-23 16:03:00 -0300

    feat: add home page to app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,20 @@
-import { useToDo } from 'contexts'
-import {
-  Header,
-  Main,
-  Section
-} from 'layouts'
-import {
-  DeleteFull,
-  Form,
-  ListToDo,
-  Title,
-  ToDo
-} from 'components'
+import { Route } from 'wouter'
+import { Home } from 'pages'
+import { Header } from 'layouts'
+import { Title } from 'components'
 
 export const App: React.FC = () => {
-  const {
-    youWillDo,
-    handleDeleteToDo,
-    handleDeleteFull
-  } = useToDo()
-
   return (
     <>
       <Header>
         <Title />
       </Header>
-      <Main>
-        <Form />
-        { youWillDo.length > 10 && <DeleteFull handleDeleteFull={ handleDeleteFull } /> }
-        <Section>
-          <ListToDo>
-            { youWillDo?.map(toDo => <ToDo key={ toDo.id } { ...toDo } handleDeleteToDo={ handleDeleteToDo } />) }
-          </ListToDo>
-        </Section>
-      </Main>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/:toDo">
+        AAA
+      </Route>
     </>
   )
 }
