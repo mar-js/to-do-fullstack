e17ae626c11061cbbe26143963ff04036d4d19aa
Martin <maguera37@gmail.com>
2023-08-02 16:03:00 -0300

    feat: add getTitleToDo in ToDo page

--- client/src/pages/to-do/index.tsx
+++ client/src/pages/to-do/index.tsx
@@ -1,8 +1,13 @@
+import { useRoute } from 'wouter'
+import { useToDo } from 'contexts'
 import { ToDoDescription } from 'components'
 
 export const ToDo: React.FC = () => {
+  const { getTitleToDo } = useToDo()
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const [ _, params ] = useRoute('/:toDo')
 
   return (
-    <ToDoDescription title="" />
+    <ToDoDescription title={ getTitleToDo(Number(params?.toDo)) } />
   )
 }
