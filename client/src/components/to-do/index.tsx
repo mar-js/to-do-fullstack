e069d50eeb058e3663e1b64b243817f6030e0fb6
Martin <maguera37@gmail.com>
2023-08-04 16:03:00 -0300

    feat: change style in ToDo component

--- client/src/components/to-do/index.tsx
+++ client/src/components/to-do/index.tsx
@@ -7,14 +7,14 @@ export const ToDo: React.FC<IToDo> = ({
   handleDeleteToDo
 }) => (
   <li className="p-5 border">
-    <h5 className="text-lg mb-5">{ toDo }</h5>
+    <h5 className="text-2xl mb-5">{ toDo }</h5>
     <div className="flex flex-col gap-2">
-      <button className="bg-red-400 text-white w-full p-2" onClick={ () => handleDeleteToDo(id) } type="button">Delete To do</button>
+      <button className="inline-block text-center w-full cursor-pointer text-md bg-red-400 p-2 text-white hover:bg-red-600" onClick={ () => handleDeleteToDo(id) } type="button">Delete To do</button>
       <Link href={ `/${id}` }>
-        <button className="inline-block text-center w-full cursor-pointer text-sm bg-gray-400 p-2 text-white hover:bg-gray-600">View To do</button>
+        <button className="inline-block text-center w-full cursor-pointer text-md bg-gray-400 p-2 text-white hover:bg-gray-600">View To do</button>
       </Link>
       <Link href={ `/edit/${id}` }>
-        <button className="inline-block text-center w-full cursor-pointer text-sm bg-green-400 p-2 text-white hover:bg-green-600">Edit To do</button>
+        <button className="inline-block text-center w-full cursor-pointer text-md bg-green-400 p-2 text-white hover:bg-green-600">Edit To do</button>
       </Link>
     </div>
   </li>
