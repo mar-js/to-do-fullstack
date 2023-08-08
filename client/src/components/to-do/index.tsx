a337cc31ce681df578620ce4d43424ce59a9b33f
Martin <maguera37@gmail.com>
2023-08-08 16:03:00 -0300

    feat: remove inline-block class in button in ToDo  component

--- client/src/components/to-do/index.tsx
+++ client/src/components/to-do/index.tsx
@@ -9,12 +9,12 @@ export const ToDo: React.FC<IToDo> = ({
   <li className="p-5 border">
     <h5 className="text-2xl mb-5">{ toDo }</h5>
     <div className="flex flex-col gap-2">
-      <button className="inline-block text-center w-full cursor-pointer text-md bg-red-400 p-2 text-white hover:bg-red-600" onClick={ () => handleDeleteToDo(id) } type="button">Delete To do</button>
+      <button className="text-center w-full cursor-pointer text-md bg-red-400 p-2 text-white hover:bg-red-600" onClick={ () => handleDeleteToDo(id) } type="button">Delete To do</button>
       <Link href={ `/${id}` }>
-        <button className="inline-block text-center w-full cursor-pointer text-md bg-gray-400 p-2 text-white hover:bg-gray-600">View To do</button>
+        <button className="text-center w-full cursor-pointer text-md bg-gray-400 p-2 text-white hover:bg-gray-600">View To do</button>
       </Link>
       <Link href={ `/edit/${id}` }>
-        <button className="inline-block text-center w-full cursor-pointer text-md bg-green-400 p-2 text-white hover:bg-green-600">Edit To do</button>
+        <button className="text-center w-full cursor-pointer text-md bg-green-400 p-2 text-white hover:bg-green-600">Edit To do</button>
       </Link>
     </div>
   </li>
