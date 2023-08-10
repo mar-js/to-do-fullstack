c895eb81778b257725ba06cecf0600d41f4966c4
Martin <maguera37@gmail.com>
2023-08-10 16:03:00 -0300

    feat: add handleClick param in ToDoButton in component

--- client/src/components/to-do-button/index.tsx
+++ client/src/components/to-do-button/index.tsx
@@ -1,5 +1,9 @@
 import { IToDoButton } from 'interfaces'
 
-export const ToDoButton: React.FC<IToDoButton> = ({ color, children }) => (
-  <button className={ `text-center w-full cursor-pointer text-md bg-${color}-400 p-2 text-white hover:bg-${color}-600` } onClick={ () => handleDeleteToDo(id) } type="button">{ children }</button>
+export const ToDoButton: React.FC<IToDoButton> = ({
+  color,
+  children,
+  handleClick
+}) => (
+  <button className={ `text-center w-full cursor-pointer text-md bg-${color}-400 p-2 text-white hover:bg-${color}-600` } onClick={ handleClick } type="button">{ children } To do</button>
 )
