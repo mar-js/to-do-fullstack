cf6d4f2191e5d3ec82359832fe90543958403372
Martin <maguera37@gmail.com>
2023-08-13 16:03:00 -0300

    fix: change color for classN in IToDoButton interface

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -25,6 +25,6 @@ export interface IToDoDescription {
 
 export interface IToDoButton extends PropsWithChildren {
   url?: string;
-  color: string;
+  classN: string;
   handleClick?: () => void;
 }
