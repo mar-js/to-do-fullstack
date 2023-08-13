0700506ad52e46c79008abf03249a267418c2291
Martin <maguera37@gmail.com>
2023-08-13 16:03:00 -0300

    fix: change color for classN in prop of ToDoButton component

--- client/src/components/to-do-button/index.tsx
+++ client/src/components/to-do-button/index.tsx
@@ -3,11 +3,11 @@ import { IToDoButton } from 'interfaces'
 
 export const ToDoButton: React.FC<IToDoButton> = ({
   url,
-  color,
+  classN,
   children,
   handleClick
 }) => (
   <Link href={ url || '' }>
-    <button className={ `text-center w-full cursor-pointer text-md bg-${color}-400 p-2 text-white hover:bg-${color}-600` } onClick={ handleClick } type="button">{ `${children} To do` }</button>
+    <button className={ `text-center w-full cursor-pointer text-md p-2 text-white ${classN}` } onClick={ handleClick } type="button">{ `${children} To do` }</button>
   </Link>
 )
