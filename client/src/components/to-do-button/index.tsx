0bea1ef723169cc8aa24b03794c7f18da07364d8
Martin <maguera37@gmail.com>
2023-08-12 22:30:58 -0300

    feat: add Link of wouter in ToDoButton component

--- client/src/components/to-do-button/index.tsx
+++ client/src/components/to-do-button/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'wouter'
 import { IToDoButton } from 'interfaces'
 
 export const ToDoButton: React.FC<IToDoButton> = ({
+  url,
   color,
   children,
   handleClick
 }) => (
-  <button className={ `text-center w-full cursor-pointer text-md bg-${color}-400 p-2 text-white hover:bg-${color}-600` } onClick={ handleClick } type="button">{ `${children} To do` }</button>
+  <Link href={ url || '' }>
+    <button className={ `text-center w-full cursor-pointer text-md bg-${color}-400 p-2 text-white hover:bg-${color}-600` } onClick={ handleClick } type="button">{ `${children} To do` }</button>
+  </Link>
 )
