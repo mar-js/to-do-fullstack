5d15b6fefc6f615ac227d688bda98b5ad013e5df
Martin <maguera37@gmail.com>
2023-08-15 16:03:00 -0300

    fix: remove handleClik prop of ToDoButton component

--- client/src/components/to-do-button/index.tsx
+++ client/src/components/to-do-button/index.tsx
@@ -4,10 +4,9 @@ import { IToDoButton } from 'interfaces'
 export const ToDoButton: React.FC<IToDoButton> = ({
   url,
   classN,
-  children,
-  handleClick
+  children
 }) => (
   <Link href={ url || '' }>
-    <button className={ `text-center w-full cursor-pointer text-md p-2 text-white ${classN}` } onClick={ handleClick } type="button">{ `${children} To do` }</button>
+    <button className={ `text-center w-full cursor-pointer text-md p-2 text-white ${classN}` } type="button">{ `${children} To do` }</button>
   </Link>
 )
