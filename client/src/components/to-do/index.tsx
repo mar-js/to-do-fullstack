0243fa43fcc171285a9c844b156147d374a0f60a
Martin <maguera37@gmail.com>
2023-08-16 16:03:00 -0300

    fix: remove handleDeleteToDo prop of ToDo component

--- client/src/components/to-do/index.tsx
+++ client/src/components/to-do/index.tsx
@@ -3,14 +3,13 @@ import { IToDo } from 'interfaces'
 
 export const ToDo: React.FC<IToDo> = ({
   id,
-  toDo,
-  handleDeleteToDo
+  toDo
 }) => (
   <li className="p-5 border">
     <h5 className="text-2xl mb-5">{ toDo }</h5>
     <div className="flex flex-col gap-2">
       { /* NOT FUNCA DELETE  */ }
-      <ToDoButton classN="bg-red-400 hover:bg-red-600" handleClick={ () => handleDeleteToDo(id) }>Delete</ToDoButton>
+      <ToDoButton classN="bg-red-400 hover:bg-red-600">Delete</ToDoButton>
       <ToDoButton classN="bg-gray-400 hover:bg-gray-600" url={ `/${id}` }>View</ToDoButton>
       <ToDoButton classN="bg-green-400 hover:bg-green-600" url={ `/edit/${id}` }>Edit</ToDoButton>
     </div>
