3959a15e4fd68edafe1b63464e5293d5025dec79
Martin <maguera37@gmail.com>
2023-08-18 16:03:00 -0300

    feat: change style of ToDoDescription component

--- client/src/components/to-do-description/index.tsx
+++ client/src/components/to-do-description/index.tsx
@@ -2,10 +2,10 @@ import { Link } from 'wouter'
 import { IToDoDescription } from 'interfaces'
 
 export const ToDoDescription: React.FC<IToDoDescription> = ({ title }) => (
-  <article className="w-full h-auto flex flex-col justify-center items-center mt-24">
+  <article className="w-4/6 h-auto flex flex-col justify-center align-start mt-14 mx-auto">
     <Link href="/">
-      <b className="cursor-pointer text-md text-gray-500">Back</b>
+      <span className="w-24 cursor-pointer text-center text-xl bg-gray-500 text-white py-2 hover:bg-gray-600">Back</span>
     </Link>
-    <h2 className="text-2xl text-gray-800 my-2">{ title }</h2>
+    <h2 className="text-3xl text-center text-gray-800 my-2">{ title }</h2>
   </article>
 )
