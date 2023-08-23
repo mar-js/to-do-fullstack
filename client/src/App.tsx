511b7018f2934f1748eeb90664a2931952a81c69
Martin <maguera37@gmail.com>
2023-08-23 10:03:00 -0300

    feat: remove all and only add home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,3 @@
-import { Route } from 'wouter'
 import { Home } from 'pages'
-import { Header } from 'layouts'
-import { Title } from 'components'
 
-export const App: React.FC = () => {
-  return (
-    <>
-      <Header>
-        <Title />
-      </Header>
-      <Route path="/">
-        <Home />
-      </Route>
-    </>
-  )
-}
+export const App: React.FC = () => <Home />
