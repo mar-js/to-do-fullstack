51fe7197be88bb17a53fbb245b20884f8e9cd777
Martin <maguera37@gmail.com>
2023-08-24 13:03:00 -0300

    feat: add header layout and title component

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,28 +1,37 @@
 import { useToDo } from 'contexts'
-import { Main, Section } from 'layouts'
+import {
+  Header,
+  Main,
+  Section
+} from 'layouts'
 import {
   DeleteFull,
   Form,
   ListToDo,
+  Title,
   ToDo
 } from 'components'
 
 export const Home: React.FC = () => {
   const {
     youWillDo,
-    handleDeleteToDo,
     handleDeleteFull
   } = useToDo()
 
   return (
-    <Main>
-      <Form />
-      { youWillDo.length > 10 && <DeleteFull handleDeleteFull={ handleDeleteFull } /> }
-      <Section>
-        <ListToDo>
-          { youWillDo?.map(toDo => <ToDo key={ toDo.id } { ...toDo } handleDeleteToDo={ handleDeleteToDo } />) }
-        </ListToDo>
-      </Section>
-    </Main>
+    <>
+      <Header>
+        <Title />
+      </Header>
+      <Main>
+        <Form />
+        { youWillDo.length > 10 && <DeleteFull handleDeleteFull={ handleDeleteFull } /> }
+        <Section>
+          <ListToDo>
+            { youWillDo?.map(toDo => <ToDo key={ toDo.id } { ...toDo } />) }
+          </ListToDo>
+        </Section>
+      </Main>
+    </>
   )
 }
