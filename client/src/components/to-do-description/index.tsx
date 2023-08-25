152dcbd225829bb4aa2e9e5ac4da89ad58c6c56f
Martin <maguera37@gmail.com>
2023-08-25 13:03:00 -0300

    feat: remove link component in ToDoDescription component

--- client/src/components/to-do-description/index.tsx
+++ client/src/components/to-do-description/index.tsx
@@ -1,11 +1,7 @@
-import { Link } from 'wouter'
 import { IToDoDescription } from 'interfaces'
 
 export const ToDoDescription: React.FC<IToDoDescription> = ({ title }) => (
   <article className="w-4/6 h-auto flex flex-col justify-center align-start mt-14 mx-auto">
-    <Link href="/">
-      <span className="w-24 cursor-pointer text-center text-xl bg-gray-500 text-white py-2 hover:bg-gray-600">Back</span>
-    </Link>
     <h2 className="text-3xl text-center text-gray-800 my-2">{ title }</h2>
   </article>
 )
