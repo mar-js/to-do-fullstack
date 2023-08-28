c87cab6372d28e68908a4db21b57e042769cab85
Martin <maguera37@gmail.com>
2023-08-28 13:03:00 -0300

    feat: add handleOpenDetails prop in ToDo component

--- client/src/components/to-do/index.tsx
+++ client/src/components/to-do/index.tsx
@@ -2,13 +2,14 @@ import { IToDo } from 'interfaces'
 
 export const ToDo: React.FC<IToDo> = ({
   id,
-  toDo
+  toDo,
+  handleOpenDetails
 }) => (
   <li className="p-5 border">
     <h5 className="text-2xl mb-5">{ toDo }</h5>
     <div className="flex flex-col gap-2">
       <button className="text-white py-2 font-semibold bg-red-400 hover:bg-red-600">Delete</button>
-      <button className="text-white py-2 font-semibold bg-gray-400 hover:bg-gray-600">View</button>
+      <button className="text-white py-2 font-semibold bg-gray-400 hover:bg-gray-600" onClick={ handleOpenDetails }>View</button>
       <button className="text-white py-2 font-semibold bg-green-400 hover:bg-green-600">Edit</button>
     </div>
   </li>
