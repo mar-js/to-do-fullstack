4576407e276a3d5fd235c0743e2f93a9bb7296c2
Martin <maguera37@gmail.com>
2023-08-31 14:03:00 -0300

    feat: add functionality of edit toDo

--- client/src/components/form/index.tsx
+++ client/src/components/form/index.tsx
@@ -1,9 +1,10 @@
-import { FormEvent } from 'react'
+import type { FC, FormEvent } from 'react'
+import { navigate } from 'wouter/use-location'
 import { useToDo } from 'contexts'
-import { IToDo } from 'interfaces'
+import { IForm, IToDo } from 'interfaces'
 
-export const Form: React.FC = () => {
-  const { handleAddToDo } = useToDo()
+export const Form: FC<IForm> = ({ id, isEdit }) => {
+  const { handleAddToDo, handleEditToDo } = useToDo()
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
@@ -12,16 +13,25 @@ export const Form: React.FC = () => {
     const DATA = Object.fromEntries(new FormData(TARGET))
     const TO_DO = DATA as unknown as IToDo
 
-    handleAddToDo({
-      ...TO_DO,
-      id: Number(new Date())
-    })
+    if (isEdit) {
+      handleEditToDo({
+        ...TO_DO,
+        id: (id as number)
+      })
+
+      navigate('/')
+    } else {
+      handleAddToDo({
+        ...TO_DO,
+        id: Number(new Date())
+      })
+    }
   }
 
   return (
-    <form action="GET" className="w-full flex justify-center items-center flex-col mb-7" onSubmit={ handleSubmit }>
+    <form action={ isEdit ? 'PUT' : 'GET' } className="w-full flex justify-center items-center flex-col mb-7" onSubmit={ handleSubmit }>
       <input type="text" placeholder="Eat" name="toDo" className="w-[60rem] p-4 text-xl border mb-5" required />
-      <button type="submit" className="bg-purple-500 text-white text-2xl px-5 py-2 rounded hover:bg-purple-800 hover:shadow-lg">Create</button>
+      <button type="submit" className="bg-purple-500 text-white text-2xl px-5 py-2 rounded hover:bg-purple-800 hover:shadow-lg">{ isEdit ? 'Edit' : 'Create' }</button>
     </form>
   )
 }
