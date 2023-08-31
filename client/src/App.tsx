bc462a2d376b288ad0643b1a277de0bedb69ca27
Martin <maguera37@gmail.com>
2023-08-31 14:03:00 -0300

    feat: add header, title, pages components and config of wouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,27 @@
-import { Home } from 'pages'
+import { Route, Switch } from 'wouter'
+import {
+  Edit,
+  Home,
+  View
+} from 'pages'
+import { Header } from 'layouts'
+import { Title } from 'components'
 
-export const App: React.FC = () => <Home />
+export const App: React.FC = () => (
+  <>
+    <Header>
+      <Title />
+    </Header>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/to-do/:id">
+        { ({ id }) => <View id={ id } /> }
+      </Route>
+      <Route path="/to-do/:id/edit">
+        { ({ id }) => <Edit id={ id } /> }
+      </Route>
+    </Switch>
+  </>
+)
