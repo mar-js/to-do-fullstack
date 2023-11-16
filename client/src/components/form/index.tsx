87df40bc8d3fe19ce8defcc6e76ecab461f1a612
Martin <maguera37@gmail.com>
2023-11-16 13:03:00 -0300

    feat: create form component

---
+++ client/src/components/form/index.tsx
@@ -0,0 +1,9 @@
+export const Form: React.FC = () => {
+
+  return (
+    <form action="GET">
+      <input type="text" placeholder="Eat" />
+      <button type="submit">Create</button>
+    </form>
+  )
+}
