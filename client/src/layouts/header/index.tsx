682e2f500b7e9e334d6e4af03b7f2ad4827d9624
Martin <maguera37@gmail.com>
2023-11-16 13:03:00 -0300

    feat: change style of header component

--- client/src/layouts/header/index.tsx
+++ client/src/layouts/header/index.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren } from 'react'
 
 export const Header: React.FC<PropsWithChildren> = ({ children }) => (
-  <header>
+  <header className="m-auto pt-10 text-center underline">
     { children }
   </header>
 )
