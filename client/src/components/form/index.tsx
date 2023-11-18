bd2dc8b8977aba6e4b991060379423c2635b4358
Martin <maguera37@gmail.com>
2023-11-18 15:03:00 -0300

    feat: add style in form component

--- client/src/components/form/index.tsx
+++ client/src/components/form/index.tsx
@@ -1,9 +1,9 @@
 export const Form: React.FC = () => {
 
   return (
-    <form action="GET">
-      <input type="text" placeholder="Eat" />
-      <button type="submit">Create</button>
+    <form action="GET" className="w-full flex justify-center items-center flex-col mb-7">
+      <input type="text" placeholder="Eat" className="w-96 p-4 text-xl border mb-5" />
+      <button type="submit" className="bg-purple-500 text-white text-2xl px-5 py-2 rounded hover:bg-purple-800 hover:shadow-lg">Create</button>
     </form>
   )
 }
