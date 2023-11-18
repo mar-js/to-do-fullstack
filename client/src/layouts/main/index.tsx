5e8a35982aea46f4f1f685bcda8cc16332689d6d
Martin <maguera37@gmail.com>
2023-11-18 15:03:00 -0300

    feat: add py-8 to main layout

--- client/src/layouts/main/index.tsx
+++ client/src/layouts/main/index.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren } from 'react'
 
 export const Main: React.FC<PropsWithChildren> = ({ children }) => (
-  <main className="w-full h-auto">
+  <main className="w-full h-auto py-8">
     { children }
   </main>
 )
