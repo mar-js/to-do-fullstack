2a8705367ee32bc053a9800651fb2c385978e347
Martin <maguera37@gmail.com>
2023-11-18 15:03:00 -0300

    feat: add main layout and form componet to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
-import { Header } from 'layouts'
-import { Title } from 'components'
+import { Header, Main } from 'layouts'
+import { Form, Title } from 'components'
 
 export const App: React.FC = () => (
   <>
     <Header>
       <Title />
     </Header>
+    <Main>
+      <Form />
+    </Main>
   </>
 )
