fedfb490052b947500fd2828d94773ff0aab93f9
Martin <maguera37@gmail.com>
2023-11-19 15:03:00 -0300

    feat: create handleSubmit function in form component and add the function to onSubmit of form tag

--- client/src/components/form/index.tsx
+++ client/src/components/form/index.tsx
@@ -1,7 +1,13 @@
+import { FormEvent } from 'react'
+
 export const Form: React.FC = () => {
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
-    <form action="GET" className="w-full flex justify-center items-center flex-col mb-7">
+    <form action="GET" className="w-full flex justify-center items-center flex-col mb-7" onSubmit={ handleSubmit }>
       <input type="text" placeholder="Eat" className="w-96 p-4 text-xl border mb-5" />
       <button type="submit" className="bg-purple-500 text-white text-2xl px-5 py-2 rounded hover:bg-purple-800 hover:shadow-lg">Create</button>
     </form>
