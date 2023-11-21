77b5882ab0ce5cb8984e99947c40c0559f77997d
Martin <maguera37@gmail.com>
2023-11-21 15:03:00 -0300

    feat: create custom class in tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [ './index.html', './src/**/*.{js,ts,jsx,tsx}' ],
-  theme: { extend: {} },
+  theme: { extend: { gridTemplateColumns: { 'auto': 'repeat(auto-fill, minmax(250px, 1fr))' } } },
   plugins: []
 }
