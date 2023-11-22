dde0cbc33a967ce6d973c2724e30c8fa953246d3
Martin <maguera37@gmail.com>
2023-11-22 16:03:00 -0300

    feat: add style in to-do component

--- client/src/components/to-do/index.tsx
+++ client/src/components/to-do/index.tsx
@@ -1,8 +1,8 @@
 import { IToDo } from 'interfaces'
 
 export const ToDo: React.FC<IToDo> = ({ id, toDo }) => (
-  <li>
-    <h5>{ toDo }</h5>
-    <a href={ `/${id}` }>View To do</a>
+  <li className="p-5 border">
+    <h5 className="text-lg mb-5">{ toDo }</h5>
+    <a href={ `/${id}` } className="inline-block text-center w-full cursor-pointer text-sm bg-gray-400 p-2 text-white rounded hover:bg-gray-600">View To do</a>
   </li>
 )
