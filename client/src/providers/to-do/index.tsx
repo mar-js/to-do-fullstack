48eb1475b8f8392271e1881cb9913588d1c0a3de
Martin <maguera37@gmail.com>
2023-11-25 16:03:00 -0300

    feat: add youWillDo state and handleAddToDo function

--- client/src/providers/to-do/index.tsx
+++ client/src/providers/to-do/index.tsx
@@ -1,8 +1,18 @@
-import type { PropsWithChildren } from 'react'
+import { useState, type PropsWithChildren } from 'react'
 import { ToDoModel } from 'contexts'
+import { IToDo, IToDoModel } from 'interfaces'
 
 export const ToDoProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const VALUE = {}
+  const [ youWillDo, setYouWillDo ] = useState([] as IToDo[])
+
+  const handleAddToDo = (toDo: IToDo) => {
+    setYouWillDo(prev => ([ ...prev, toDo ]))
+  }
+
+  const VALUE: IToDoModel = {
+    youWillDo,
+    handleAddToDo
+  }
 
   return (
     <ToDoModel.Provider value={ VALUE }>
