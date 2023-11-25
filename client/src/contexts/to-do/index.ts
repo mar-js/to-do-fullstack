6439dedec80032f81d34f3f2febe84cb27832c23
Martin <maguera37@gmail.com>
2023-11-25 16:03:00 -0300

    feat: add to-do-model interface to to-do context

--- client/src/contexts/to-do/index.ts
+++ client/src/contexts/to-do/index.ts
@@ -1,4 +1,5 @@
 import { createContext, useContext } from 'react'
+import { IToDoModel } from 'interfaces'
 
-export const ToDoModel = createContext({})
+export const ToDoModel = createContext({} as IToDoModel)
 export const useToDo = () => useContext(ToDoModel)
